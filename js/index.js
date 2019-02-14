425d0d903f1faa097c8a3524613c898a59b95ed5
Pamela Frieze <pfrieze@gmail.com>
2019-02-14 16:01:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,9 +5,14 @@ function greet(customerOne, customerTwo) {
  
 let sally = { name: 'Sally' };
  
-function greet(customerOne, customerTwo) {
-    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
 }
  
-greet.call(sally, 'Terry', 'George');
-// Hi Terry and George, my name is Sally!
+let newGreet = greet.bind(sally); // newGreet is context-bound to sally
+ 
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+ 
+greet('Bob');
+// Hi Bob, my name is !
